a8054a82761f87958cb7d459bf7b7010b925e118
blueworrybear <blueworrybear@gmail.com>
2014-05-09 13:05:35 +0800

    bear

--- src/cachedataanalysis/CacheDataAnalysis.java
+++ src/cachedataanalysis/CacheDataAnalysis.java
@@ -40,7 +40,7 @@ public class CacheDataAnalysis {
         int size = 16;
         
         //bear
-       
+       //new
         
         //FlowTableWindow flowWindow = new FlowTableWindow(new File("flowlog.txt"));
         FlowTableWindow flowWindow = null;
