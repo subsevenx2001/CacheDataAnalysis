9dfa0faa7d20d25cb160e696d25bc4890aa6346a
blueworrybear <blueworrybear@gmail.com>
2014-05-12 10:44:14 +0800

    remote

--- src/cachedataanalysis/CacheDataAnalysis.java
+++ src/cachedataanalysis/CacheDataAnalysis.java
@@ -40,7 +40,7 @@ public class CacheDataAnalysis {
         int size = 16;
         
         //bear
-       //new remote
+       //new r
         
         //FlowTableWindow flowWindow = new FlowTableWindow(new File("flowlog.txt"));
         FlowTableWindow flowWindow = null;
